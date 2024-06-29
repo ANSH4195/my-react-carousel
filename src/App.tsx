f532be1086ed5a3a32a3da990d63581e98ee28bb
Ansh Shrivastava <shri.ansh4563@gmail.com>
2024-06-29 19:34:31 +0100

    Add vertical mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import CarouselContainer from "./Carousel/Container";
 
 function App() {
+	const [isVertical, setIsVertical] = useState(false);
+
 	return (
 		<div className="container mx-auto py-12">
-			<CarouselContainer />
+			<button
+				type="button"
+				className="border-2 py-2 px-4 my-3 rounded-xl bg-slate-200 active:bg-white transition-all duration-50 ease-in-out"
+				onClick={(e) => {
+					e.preventDefault();
+					setIsVertical((prev) => !prev);
+				}}
+			>
+				Toggle Vertical Mode
+			</button>
+
+			<CarouselContainer isVertical={isVertical} />
 		</div>
 	);
 }
